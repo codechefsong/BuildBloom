9e58df47cc9f972bb3fadabd1877ee5609840bc0
Codechef Song <133816663+codechefsong@users.noreply.github.com>
2024-03-15 16:51:11 -0400

    add a house icon

--- packages/nextjs/app/map/_components/Map.tsx
+++ packages/nextjs/app/map/_components/Map.tsx
@@ -2,9 +2,17 @@ import { useState } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
 
 const userIcon = new L.Icon({
   iconUrl: "/user.png",
+  iconSize: [30, 40],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
+
+const houseIcon = new L.Icon({
+  iconUrl: "/house.png",
   iconSize: [30, 40],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
@@ -19,6 +27,11 @@ const Map = () => {
   const [position, setPosition] = useState<Coordinates>({ lat: 51.505, lng: -0.09 });
   console.log(setPosition);
 
+  const { data: buildings } = useScaffoldContractRead({
+    contractName: "BuildBloom",
+    functionName: "getBuildings",
+  });
+
   return (
     <>
       <div>
@@ -28,6 +41,9 @@ const Map = () => {
             url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           <Marker position={[position.lat, position.lng]} icon={userIcon} />
+          {buildings?.map((b, index) => (
+            <Marker key={index} position={[Number(b.lat), Number(b.lng)]} icon={houseIcon} />
+          ))}
         </MapContainer>
       </div>
     </>
