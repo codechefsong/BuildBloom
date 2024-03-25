978a60ec3ab92db126ff4c8b46dbaf24bce10d4b
Codechef Song <133816663+codechefsong@users.noreply.github.com>
2024-03-25 16:02:11 -0400

    change the home page

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,65 +1,49 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
-
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">Build Bloom</span>
           </h1>
-          <div className="flex justify-center items-center space-x-2">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
+          <Image className="ml-8" alt="Game" width={450} height={350} src="/game.png" />
+          <p className="text-center text-lg mb-0">
+            Explore the world to gather resources and build your own virtual town
+          </p>
+          <div className="flex justify-center mb-2">
+            <Link
+              href="/map"
+              passHref
+              className=" py-2 px-16 mb-1 mt-3 bg-green-500 rounded baseline hover:bg-green-400 disabled:opacity-50"
+            >
+              Explore
+            </Link>
           </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
+        <div className="flex-grow bg-yellow-100 w-full mt-16 px-8 py-12">
+          <div className="text-center">
+            <h2 className="mt-3 text-4xl mb-5">Gameplay</h2>
+          </div>
+          <div className="flex justify-center">
+            <div className="w-[700px]">
+              <ul className="list-disc text-xl" style={{ width: "600px" }}>
+                <li>It utilizes real-world location tracking similar to Pokémon GO</li>
+                <li>Make sure your GPS is enabled to accurately track your movements</li>
+                <li>Walk or travel to the designated locations on the map to gather resources</li>
+                <li>
+                  Once you&apos;re within range of a resource hotspot, tap on it to collect wood, cement, or glass
+                </li>
+                <li>Locate empty plots of land on the map where you wish to construct houses</li>
+                <li>Successfully constructing a house earns you points</li>
+              </ul>
             </div>
           </div>
         </div>
